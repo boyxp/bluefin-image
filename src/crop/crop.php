53237dcd594f99a191c936ca9301bb74b6d7c426
boyxp <isme@live.com>
2016-04-13 21:38:10 +0800

    新增crop接口

---
+++ src/crop/crop.php
@@ -0,0 +1,8 @@
+<?php
+namespace bluefin\image\crop;
+interface crop
+{
+	public function setZoom(bool $zoom):bool;
+	public function setCoord(int $x=0, int $y=0):bool;
+	public function setSize(int $width, int $height):bool;
+}
